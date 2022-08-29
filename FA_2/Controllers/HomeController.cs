d89ab4ec4282e0e11516045046624ccdbbfa91f4
Roelof Kristensen <89071802+RoelofKristensen@users.noreply.github.com>
2022-08-29 10:43:37 +0200

    Push new project

---
+++ FA_2/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FA_2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Location()
+        {
+            return View();
+        }
+
+        public ActionResult Favorites()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "If you want to know more contact us at 0123456789";
+
+            return View();
+        }
+
+        public ActionResult Latest_News()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return Redirect("https://screenrant.com/comics/"); ;
+        }
+    }
+}
---
+++ FA_2/obj/Release/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FA_2.MvcApplication" Language="C#" %>
